d0754c8c9b90ba2117e4f119a9edbd8aa89fb7f5
darrooo <daniella.rosendahl@telia.com>
2020-11-27 17:05:12 +0100

    testar igen

---
+++ public/js/menu.js
@@ -0,0 +1,60 @@
+
+//menyn till listan 
+
+var food = [
+  {
+  "name": "Invisible Burger",
+  "kCal": -850,
+  "disc1":"Fits everyone (no flavour)",
+  "disc2":"Plenty of oxygen",
+  "lactose": true,
+  "gluten": false,
+  "img": "https://th.bing.com/th/id/OIP.yh8cGkmaSfITmeP9QTznvwAAAA?pid=Api&w=329&h=292&rs=1",
+  },
+  {
+  "name": "Unicorn Burger",
+  "kCal": 840,
+  "disc1":"Happiness flavour",
+  "disc2":"Might give colorful burps",
+  "lactose": false,
+  "gluten": false,
+  "img": "https://i.pinimg.com/736x/d1/15/cf/d115cff9947a5e4a8d031b22ae2ef7ca.jpg",
+  },
+  {
+  "name": "Goth Burger",
+  "kCal": 0,
+  "disc1":"Dark side flavour",
+  "disc2":"Might bring he-who-must-not-be-named to life",
+  "lactose": false,
+  "gluten": true,
+  "img": "https://cbsnews1.cbsistatic.com/hub/i/2015/10/09/f7330b1f-d3a1-48b6-8db9-fe35fa400049/burger-king-halloween-black-burger.jpg",
+  },
+  {
+  "name": "Sloppy Burger",
+  "kCal": 45680,
+  "disc1":"Overly-manly-man",
+  "disc2":"Gives respect",
+  "lactose": true,
+  "gluten": true,
+  "img": "https://www.foodiecrush.com/wp-content/uploads/2011/09/One-Pot-Sloppy-Joes-foodiecrush.com-013-1-500x375.jpg",
+},
+  {
+  "name": "Flying Burger",
+  "kCal": 850,
+  "disc1":"Free delivery, open your window!",
+  "disc2":"Might loose some part on the way, no reimbursement",
+  "lactose": true,
+  "gluten": false,
+  "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwdjLpkHxWc1kU1DisHMEk1UZAXaw5oFizKg&usqp=CAU",
+},
+  {
+  "name": "I-just-wanted-to-FIKA Burger",
+  "kCal": 400,
+  "disc1":"Suger rush on the way",
+  "disc2":"can we leave now?",
+  "lactose": false,
+  "gluten": false,
+  "img": "https://images.unsplash.com/photo-1558326567-98ae2405596b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+},
+
+]
